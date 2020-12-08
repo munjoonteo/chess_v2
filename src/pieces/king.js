164a691d499c13960bd37d5b617319780661c691
munjoonteo <teomunjoon101@gmail.com>
2020-12-08 12:52:00 +1100

    fixed castling

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -11,17 +11,10 @@ class King extends Piece {
       for (let j = -1; j < 2; j++) {
         let move = [this.xGrid + i, this.yGrid + j];
 
-        let opponent;
-        if (this.colour === "white") {
-          opponent = this.board.black;
-        } else {
-          opponent = this.board.white;
-        }
-
         if (
           this.withinBounds(move) &&
           board.boardState[move[1]][move[0]].colour != this.colour &&
-          !this.moveIntoCheck(move[0], move[1], opponent)
+          !this.board.moveIntoCheck(move[0], move[1])
         ) {
           moveset.push(move);
         }
@@ -38,25 +31,30 @@ class King extends Piece {
       return;
     }
 
-    let curr, opponent;
+    let right;
+    if (finalX > this.originalX) {
+      right = true;
+    } else {
+      right = false;
+    }
+
+    let curr;
     if (this.colour === "white") {
       curr = this.board.white;
-      opponent = this.board.black;
     } else {
       curr = this.board.black;
-      opponent = this.board.white;
     }
 
     let success = false;
     for (let piece of curr) {
       if (piece.name === "R" && piece.yGrid == this.yGrid && !piece.hasMoved) {
-        if (finalX < piece.x) {
+        if (finalX < piece.x && right) {
           // Right castle
           let isEmpty = true;
           for (let i = this.xGrid + 1; i < dimensions - 1; i++) {
             if (
               this.board.boardState[this.yGrid][i].colour !== "" ||
-              this.moveIntoCheck(i, this.yGrid, opponent)
+              this.board.moveIntoCheck(i, this.yGrid)
             ) {
               isEmpty = false;
             }
@@ -65,21 +63,17 @@ class King extends Piece {
           if (!isEmpty) continue;
 
           this.movePiece(finalX, finalY);
-
-          if (this.colour === "white") {
-            piece.movePiece(piece.x - 2 * squareWidth, piece.y);
-          } else {
-            piece.movePiece(piece.x + 3 * squareWidth, piece.y);
-          }
+          piece.movePiece(piece.x - 2 * squareWidth, piece.y);
 
           success = true;
-        } else {
+          break;
+        } else if (finalX > piece.x && !right) {
           // Left castle
           let isEmpty = true;
           for (let i = this.xGrid - 1; i > 1; i--) {
             if (
               this.board.boardState[this.yGrid][i].colour !== "" ||
-              this.moveIntoCheck(i, this.yGrid, opponent)
+              this.board.moveIntoCheck(i, this.yGrid)
             ) {
               isEmpty = false;
             }
@@ -88,14 +82,10 @@ class King extends Piece {
           if (!isEmpty) continue;
 
           this.movePiece(finalX, finalY);
-
-          if (this.colour === "white") {
-            piece.movePiece(piece.x + 3 * squareWidth, piece.y);
-          } else {
-            piece.movePiece(piece.x - 2 * squareWidth, piece.y);
-          }
+          piece.movePiece(piece.x + 3 * squareWidth, piece.y);
 
           success = true;
+          break;
         }
       }
     }
@@ -103,16 +93,8 @@ class King extends Piece {
     if (!success) {
       this.x = this.originalX;
       this.y = this.originalY;
-    }
-  }
-
-  moveIntoCheck(xGrid, yGrid, opponent) {
-    for (let piece of opponent) {
-      for (let move of piece.moveset) {
-        if (xGrid == move[0] && yGrid == move[1]) {
-          return true;
-        }
-      }
+    } else {
+      this.board.nextTurn();
     }
   }
 }
